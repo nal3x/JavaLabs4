e9f844b2f4327665e92c326212b2d9ccff73a902
nn <nal3x@users.noreply.github.com>
2017-10-31 12:30:20 +0200

    Update TestArrayList1.java

--- src/TestArrayList1.java
+++ src/TestArrayList1.java
@@ -1,1 +1,20 @@
+import java.util.ArrayList;
 
+public class TestArrayList1 {
+    
+  public static void main ( String [ ] args ) {
+    ArrayList <String> list1 = new ArrayList <String>() ;
+
+    // προσθήκη στοιχείων στη λίστα
+    for ( int i = 5; i > 0; i-- ) {
+        list1.add (0, "List item " + i ) ;
+    }
+	
+    // εκτύπωση λίστας
+    System.out.println ( "list1 size: " + list1.size ( ) ) ;
+
+    for ( int i = 0; i < list1.size ( ) ; i++ ) {
+        System.out.println ( list1.get ( i ) ) ;
+    }
+  } 
+}
