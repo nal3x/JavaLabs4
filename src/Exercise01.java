2da7899edb49a8f1506f5df3f661d37c0d009c68
nalex <nalex@e7450>
2017-10-31 14:11:59 +0200

    .

---
+++ src/Exercise01.java
@@ -0,0 +1,23 @@
+import java.util.*
+
+public class Exercise01 {
+    
+    public static void main(String[] args)  {
+       
+       //your code here
+        
+       System.out.println("Αρχική μορφή της λίστας: "); 
+       printList(list); 
+       scale(list);
+       System.out.println("Η λίστα μετά την κλήση της μεθόδου: "); 
+       printList(list); 
+    } 
+    
+    public static void printList(ArrayList <Integer> list) { 
+        //your code here 
+    } 
+
+    public static void scale(ArrayList<Integer> list) { 
+        //your code here
+    } 
+}
